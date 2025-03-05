b13ad9b5e99d7f7b146ea2f9d1523520c106ad2e
jjd3109 <jongdoo2001@naver.com>
2025-03-05 17:56:25 +0900

    fix: producer 전달 실패시 에러 메시지 생성

--- src/main/java/com/example/simplekafkaredisrdmbs/kafka/ViewCountProducer.java
+++ src/main/java/com/example/simplekafkaredisrdmbs/kafka/ViewCountProducer.java
@@ -4,15 +4,22 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ViewCountProducer {
 
 	private final KafkaTemplate<String, String> kafkaTemplate;
 	private static final String TOPIC = "view-count";
 
 	public void send(String contentId){
-		kafkaTemplate.send(TOPIC, contentId);
+		try{
+			kafkaTemplate.send(TOPIC, contentId);
+		}catch(Exception e){
+			log.info("에러 : {} ", e.getMessage());
+		}
+
 	}
 }
